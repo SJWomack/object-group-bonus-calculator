6e54583e04d0c235e9c1db06229a078c8f12090b
Joey Lundquist <joeylundquist91@gmail.com>
2022-05-10 15:24:19 -0500

    Finished what we could in the time allotted

--- client.js
+++ client.js
@@ -39,4 +39,75 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+function newObject(array) {
+  let person = {}
+  let bonusArray =[]
+  for (let info of array) {
+    person = {
+      name: '',
+      bonusPercentage: 0,
+      totalCompensation: 0,
+      totalBonus: 0
+    }
+    if (info.reviewRating === 5) {
+      person.bonusPercentage = 0.1;
+    }
+    else if (info.reviewRating === 4) {
+      person.bonusPercentage = 0.06;
+    }
+    else if (info.reviewRating === 3) {
+      person.bonusPercentage = 0.04;
+    }
+    else {
+      person.bonusPercentage = 0;
+    }
+    if (info.employeeNumber.length === 4) {
+     person.bonusPercentage += 0.05;
+    }
+    if (info.annualSalary > 65000) {
+      person.bonusPercentage -= 0.01;
+    }
+    if (person.bonusPercentage > 0.13){
+      person.bonusPercentage = 0.13;
+    }
+    else if (person.bonusPercentage < 0){
+      person.bonusPercentage = 0;
+    }
+     person.name = info.name;
+     person.totalBonus =Number(info.annualSalary * person.bonusPercentage);
+     person.totalCompensation = Number(info.annualSalary) + person.totalBonus;
+
+   bonusArray.push(person);
+
+  }
+
+  return bonusArray;
+   
+
+}
+console.log(newObject(employees));
+
+/*function yearsEmployed() {
+  let employeeNum = 0;
+  for (let tacos of employees) {
+    let employeeNum = tacos.employeeNumber;
+    
+  }
+  return employeeNum;
+}
+console.log(yearsEmployed(employees));
+/*function bonuses(array){
+  let salRev;
+    for(let employee of array) {
+      salRev = {
+        salary: employee.annualSalary,
+        review: employee.reviewRating
+      }
+      console.log(salRev)
+    }
+    return salRev
+  }*/
+
+
